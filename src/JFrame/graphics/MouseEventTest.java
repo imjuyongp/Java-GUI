fd482528ffe9b33cf92146353c950cef47a9c313
imjuyongp <imjuyongp@gmail.com>
2025-10-29 17:42:04 +0900

    mouseAdapter() 상속하여 필요한 것만 재정의

--- src/JFrame/graphics/MouseEventTest.java
+++ src/JFrame/graphics/MouseEventTest.java
@@ -2,6 +2,7 @@ package JFrame.graphics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -19,12 +20,26 @@ public class MouseEventTest extends JFrame {
     int x,y,w,h;
   }
 
-  class MyPanel extends JPanel implements MouseListener {
+  class MyPanel extends JPanel {
     Rectangle[] array =  new Rectangle[5]; // 화면에 그리는 사각형을 저장하는 배열 객체
     int index = 0;
 
     public MyPanel() {
-      this.addMouseListener(this);
+      this.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mousePressed(MouseEvent e) {
+          if(index > 5) {
+            return; // 배열의 크기를 넘어서면 종료
+          }
+          array[index] = new Rectangle();
+          array[index].x = e.getX();
+          array[index].y = e.getY();
+          array[index].w = 50;
+          array[index].h = 50;
+          index++;
+          repaint();
+        }
+      });
     }
 
     public void paintComponent(Graphics g) {
@@ -35,29 +50,6 @@ public class MouseEventTest extends JFrame {
         }
       }
     }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-      if(index > 5) {
-        return; // 배열의 크기를 넘어서면 종료
-      }
-      array[index] = new Rectangle();
-      array[index].x = e.getX();
-      array[index].y = e.getY();
-      array[index].w = 50;
-      array[index].h = 50;
-      index++;
-      repaint();
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-    @Override
-    public void mouseClicked(MouseEvent e) {}
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-    @Override
-    public void mouseExited(MouseEvent e) {}
 
   }
 
